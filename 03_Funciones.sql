7254994018971f093e43e21ea30b0aef34645c0c
BrayanStevenRianoFuentesADSO <brayanstevenriano@gmail.com>
2026-09-14 13:52:18 -0500

    feat: agregar funciones

--- 03_Funciones.sql
+++ 03_Funciones.sql
@@ -11,7 +11,52 @@ BEGIN
     FROM detalle_ventas
     WHERE id_venta = p_id_venta;
 
-    RETURN COALESCE(total_venta, 0);
+    RETURN total_venta;
 END //
 
+-- fn_VerificarDisponibilidadStock: Valida si hay stock suficiente para un producto.
+
+CREATE FUNCTION fn_VerificarDisponibilidadStock(
+    p_id_producto INT,
+    p_stock_solicitado INT
+)
+
+RETURNS Varchar(50)
+DETERMINISTIC
+
+BEGIN
+    DECLARE stock_total INT;
+
+    SELECT stock
+    INTO stock_total
+    FROM productos
+    WHERE id_producto = p_id_producto;
+
+    if p_stock_solicitado <=stock_total then
+    return concat("stock suficiente, el stock total disponible es de ", stock_total);
+    
+    else
+    return concat("stock insuficiente el stock total disponible es de ", stock_total);
+    end if;
+END //
+
+
+-- fn_ObtenerPrecioProducto: Devuelve el precio actual de un producto.
+
+CREATE function fn_ObtenerPrecioProducto(
+
+p_id_producto int
+)
+
+deterministic
+
+begin
+	select precio from 
+end
+
 DELIMITER ;
+
+
+SELECT fn_VerificarDisponibilidadStock(1, 16);
+
+
