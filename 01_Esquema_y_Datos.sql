8aeeb6462386f1d56af0a2c1b19a7d717957e8e6
BrayanStevenRianoFuentesADSO <brayanstevenriano@gmail.com>
2026-09-15 13:52:49 -0500

    feat: agregar tablas auditorias, promociones

--- 01_Esquema_y_Datos.sql
+++ 01_Esquema_y_Datos.sql
@@ -14,7 +14,8 @@ region varchar(100) not null,
 email varchar(100) not null UNIQUE,
 contrasenia varchar(100) not null,
 direccion_envio varchar(100),
-fecha_registro datetime DEFAULT CURRENT_TIMESTAMP
+fecha_registro datetime DEFAULT CURRENT_TIMESTAMP,
+total_gastado decimal(10,2) default 0
 );
 
 CREATE table categorias(
@@ -39,6 +40,7 @@ CREATE table productos(
     stock int DEFAULT 0 CHECK(stock>=0),
     sku varchar(50) not null unique,
     fecha_creacion datetime default current_timestamp,
+    fecha_modificacion datetime default current_timestamp,
     activo boolean default True,
     id_categoria int not null,
     id_proveedor int not null,
@@ -109,16 +111,33 @@ CREATE TABLE vistas_productos (
     FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
 
+CREATE TABLE auditoria_precios (
+    id_auditoria INT AUTO_INCREMENT PRIMARY KEY,
+    id_producto INT,
+    precio_anterior DECIMAL(10,2),
+    precio_nuevo DECIMAL(10,2),
+    fecha_modificacion DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 5 
+CREATE TABLE auditoria_clientes (
+    id_auditoria INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT,
+    nombres VARCHAR(100),
+    apellidos VARCHAR(100),
+    email VARCHAR(100),
+    fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
 INSERT INTO clientes
-(nombres, apellidos, fecha_nacimiento, ciudad, region, email, contrasenia, direccion_envio)
+(nombres, apellidos, fecha_nacimiento, ciudad, region, email, contrasenia, direccion_envio, total_gastado)
 VALUES
-('Ana', 'Gomez', '1998-05-12', 'Bucaramanga', 'Santander', 'ana.gomez@gmail.com', 'ana123', 'Calle 45 #12-30'),
-('Luis', 'Martinez', '1995-08-20', 'Bogota', 'Cundinamarca', 'luis.martinez@gmail.com', 'luis123', 'Carrera 10 #20-15'),
-('Marta', 'Rodriguez', '2000-02-10', 'Medellin', 'Antioquia', 'marta.rodriguez@gmail.com', 'marta123', 'Calle 50 #30-20'),
-('Carlos', 'Perez', '1997-11-25', 'Cali', 'Valle del Cauca', 'carlos.perez@gmail.com', 'carlos123', 'Carrera 5 #15-40'),
-('Sofia', 'Torres', '2001-07-18', 'Barranquilla', 'Atlantico', 'sofia.torres@gmail.com', 'sofia123', 'Calle 80 #10-25'),
-('Diego', 'Hernandez', '1994-03-30', 'Cartagena', 'Bolivar', 'diego.hernandez@gmail.com', 'diego123', 'Carrera 2 #30-10');
+('Ana', 'Gomez', '1998-05-12', 'Bucaramanga', 'Santander', 'ana.gomez@gmail.com', 'ana123', 'Calle 45 #12-30',10000),
+('Luis', 'Martinez', '1995-08-20', 'Bogota', 'Cundinamarca', 'luis.martinez@gmail.com', 'luis123', 'Carrera 10 #20-15',213131),
+('Marta', 'Rodriguez', '2000-02-10', 'Medellin', 'Antioquia', 'marta.rodriguez@gmail.com', 'marta123', 'Calle 50 #30-20',111123),
+('Carlos', 'Perez', '1997-11-25', 'Cali', 'Valle del Cauca', 'carlos.perez@gmail.com', 'carlos123', 'Carrera 5 #15-40',1999999),
+('Sofia', 'Torres', '2001-07-18', 'Barranquilla', 'Atlantico', 'sofia.torres@gmail.com', 'sofia123', 'Calle 80 #10-25',200000),
+('Diego', 'Hernandez', '1994-03-30', 'Cartagena', 'Bolivar', 'diego.hernandez@gmail.com', 'diego123', 'Carrera 2 #30-10',3021232);
 
 INSERT INTO categorias
 (id_categoria, nombre, descripcion)
@@ -139,19 +158,27 @@ VALUES
 (5, 'Global Tech', 'info@globaltech.com', '3045678901');
 
 INSERT INTO productos
-(nombre, descripcion, precio, costo, stock, sku, activo, id_categoria, id_proveedor, stock_minimo, ubicacion, peso)
+(nombre, descripcion, precio, costo, stock, sku, activo, id_categoria, id_proveedor, stock_minimo, ubicacion, peso, fecha_creacion, fecha_modificacion)
 VALUES
-('Laptop Lenovo IdeaPad', 'Laptop Lenovo para trabajo y estudio', 2500000.00, 2000000.00, 15, 'LAP-001', TRUE, 1, 1, 5, 'A1', 1.80),
-('Laptop HP Pavilion', 'Laptop HP para uso profesional', 3200000.00, 2700000.00, 10, 'LAP-002', TRUE, 1, 2, 3, 'A2', 1.70),
-('Mouse Logitech', 'Mouse inalambrico Logitech', 85000.00, 50000.00, 50, 'MOU-001', TRUE, 2, 1, 10, 'B1', 0.10),
-('Teclado Redragon', 'Teclado mecanico para gaming', 220000.00, 150000.00, 30, 'TEC-001', TRUE, 2, 3, 5, 'B2', 0.80),
-('iPhone 15', 'Smartphone Apple iPhone 15', 4200000.00, 3500000.00, 8, 'IPH-001', TRUE, 3, 4, 2, 'C1', 0.17),
-('Samsung Galaxy S24', 'Smartphone Samsung Galaxy S24', 3500000.00, 2900000.00, 12, 'SAM-001', TRUE, 3, 4, 3, 'C2', 0.16),
-('Audifonos Sony', 'Audifonos inalambricos Sony', 450000.00, 320000.00, 25, 'AUD-001', TRUE, 4, 5, 5, 'D1', 0.25),
-('Parlante JBL', 'Parlante Bluetooth JBL', 380000.00, 280000.00, 20, 'PAR-001', TRUE, 4, 5, 4, 'D2', 0.70),
-('SSD Kingston 1TB', 'Unidad de almacenamiento SSD de 1TB', 350000.00, 250000.00, 35, 'SSD-001', TRUE, 5, 2, 8, 'E1', 0.10),
-('Memoria USB 128GB', 'Memoria USB de 128GB', 70000.00, 40000.00, 60, 'USB-001', TRUE, 5, 3, 15, 'E2', 0.02);
+('Laptop Lenovo IdeaPad', 'Laptop Lenovo para trabajo y estudio', 2500000.00, 2000000.00, 15, 'LAP-001', TRUE, 1, 1, 5, 'A1', 1.80, NOW(), NOW()),
 
+('Laptop HP Pavilion', 'Laptop HP para uso profesional', 3200000.00, 2700000.00, 10, 'LAP-002', TRUE, 1, 2, 3, 'A2', 1.70, NOW(), NOW()),
+
+('Mouse Logitech', 'Mouse inalambrico Logitech', 85000.00, 50000.00, 50, 'MOU-001', TRUE, 2, 1, 10, 'B1', 0.10, NOW(), NOW()),
+
+('Teclado Redragon', 'Teclado mecanico para gaming', 220000.00, 150000.00, 30, 'TEC-001', TRUE, 2, 3, 5, 'B2', 0.80, NOW(), NOW()),
+
+('iPhone 15', 'Smartphone Apple iPhone 15', 4200000.00, 3500000.00, 8, 'IPH-001', TRUE, 3, 4, 2, 'C1', 0.17, NOW(), NOW()),
+
+('Samsung Galaxy S24', 'Smartphone Samsung Galaxy S24', 3500000.00, 2900000.00, 12, 'SAM-001', TRUE, 3, 4, 3, 'C2', 0.16, NOW(), NOW()),
+
+('Audifonos Sony', 'Audifonos inalambricos Sony', 450000.00, 320000.00, 25, 'AUD-001', TRUE, 4, 5, 5, 'D1', 0.25, NOW(), NOW()),
+
+('Parlante JBL', 'Parlante Bluetooth JBL', 380000.00, 280000.00, 20, 'PAR-001', TRUE, 4, 5, 4, 'D2', 0.70, NOW(), NOW()),
+
+('SSD Kingston 1TB', 'Unidad de almacenamiento SSD de 1TB', 350000.00, 250000.00, 35, 'SSD-001', TRUE, 5, 2, 8, 'E1', 0.10, NOW(), NOW()),
+
+('Memoria USB 128GB', 'Memoria USB de 128GB', 70000.00, 40000.00, 60, 'USB-001', TRUE, 5, 3, 15, 'E2', 0.02, NOW(), NOW());
 INSERT INTO ventas
 (id_venta, fecha_venta, estado, total, id_cliente)
 VALUES
@@ -230,7 +257,6 @@ VALUES
 (37, 1, 3200000.00, 2, 30),
 (38, 1, 4200000.00, 5, 31),
 (39, 1, 3500000.00, 6, 32);
-
 
 INSERT INTO carritos
 (id_cliente, fecha_creacion, estado)
@@ -312,3 +338,5 @@ VALUES
 (10, 3, '2026-05-10 10:00:00'),
 (10, 4, '2026-05-11 11:20:00'),
 (10, 5, '2026-05-12 12:40:00');
+
+
